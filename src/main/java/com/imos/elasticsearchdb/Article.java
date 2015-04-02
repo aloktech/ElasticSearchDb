ad4a5828da017fe550419e7949de3acb66e29fbf
aloktech <meher.ranjan@gmail.com>
2015-04-02 07:53:49 +0530

    New Project

---
+++ src/main/java/com/imos/elasticsearchdb/Article.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.imos.elasticsearchdb;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Alok
+ */
+@Data
+public class Article {
+    
+    private String author, content;
+}
---
+++ src/main/java/com/imos/elasticsearchdb/NewClass.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.imos.elasticsearchdb;
+
+import static com.imos.elasticsearchdb.utils.ElasticSearchDBConstants.*;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Alok
+ */
+public class NewClass {
+
+    NewClass() {
+        String query = null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream("com/imos/sample/query.json")));
+        String line = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\n");
+            }
+            query = builder.toString();
+            System.out.println(query);
+        } catch (IOException ex) {
+            Logger.getLogger(NewClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Construct a new Jest client according to configuration via factory
+        String connectionUrl = "http://localhost:9200";
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(new HttpClientConfig.Builder(connectionUrl)
+                .multiThreaded(true)
+                .build());
+        JestClient client = factory.getObject();
+
+//        new CreateIndex.Builder("articles").build();
+//        try {
+//            client.execute(new CreateIndex.Builder("articles").build());
+//        } catch (Exception ex) {
+//            Logger.getLogger(NewClass.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        Article source = new Article();
+        source.setAuthor("John Ronald Reuel Tolkien");
+        source.setContent("The Lord of the Rings is an epic high fantasy novel");
+
+        Index index = new Index.Builder(source).index("articles").type("article").build();
+//        try {
+//            client.execute(index);
+//        } catch (Exception ex) {
+//            Logger.getLogger(NewClass.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+        Search search = (Search) new Search.Builder(query)
+                // multiple index or types can be added.
+                .addIndex("articles")
+                .addType("article")
+                .build();
+
+        JestResult result = null;
+        try {
+            result = client.execute(search);
+            System.out.println(result.getJsonString());
+        } catch (Exception ex) {
+            Logger.getLogger(NewClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        JSONObject hits = getResults(result.getJsonString());
+        if (hits.has(HITS)) {
+            JSONArray array = hits.getJSONArray(HITS);
+            
+        }
+        
+    }
+
+    public static void main(String[] args) {
+
+        NewClass newClass = new NewClass();
+    }
+
+    public final JSONObject getResults(String result) {
+        JSONObject root = new JSONObject(result);
+        if (root.has(HITS)) {
+            return root.getJSONObject(HITS);
+        }
+
+        return root;
+    }
+
+}
